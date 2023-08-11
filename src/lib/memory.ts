bd53f4e38236933b158a35d1c1c4740a4104b38e
Vignesh <vighneshgupta32@gmail.com>
2023-08-11 23:44:47 +0530

    [added] required package

---
+++ src/lib/memory.ts
@@ -0,0 +1,79 @@
+import { Redis } from "@upstash/redis";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+export type CompanionKey = {
+  companionName: string;
+  modelName: string;
+  userId: string;
+};
+
+export class MemoryManager {
+  private static instance: MemoryManager;
+  private history: Redis;
+  private vectorDBClient: PineconeClient;
+
+  public constructor() {
+    this.history = Redis.fromEnv();
+    this.vectorDBClient = new PineconeClient();
+  }
+
+  public async init() {
+    if (this.vectorDBClient instanceof PineconeClient) {
+      await this.vectorDBClient.init({
+        apiKey: process.env.PINECONE_API_KEY!,
+        environment: process.env.PINECONE_ENVIRONMENT!,
+      });
+    }
+  }
+
+  public async vectorSearch(
+    recentChatHistory: string,
+    companionFileName: string
+  ) {
+    const pineconeClient = <PineconeClient>this.vectorDBClient;
+
+    const pineconeIndex = pineconeClient.Index(process.env.PINECONE_INDEX!);
+
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY! }),
+      { pineconeIndex }
+    );
+    const similarDocs = await vectorStore
+      .similaritySearch(recentChatHistory, 3, { __filename: companionFileName })
+      .catch((err) => {
+        console.log("FAILED TO SEARCH FOR SIMILAR DOCS", err);
+      });
+
+    return similarDocs;
+  }
+
+  public static async getInstance(): Promise<MemoryManager> {
+    if (!MemoryManager.instance) {
+      MemoryManager.instance = new MemoryManager();
+      await MemoryManager.instance.init();
+    }
+
+    return MemoryManager.instance;
+  }
+
+  public async generateRedisCompanionKey(companionKey: CompanionKey) {
+    return `${companionKey.companionName}-${companionKey.modelName}-${companionKey.userId}`;
+  }
+
+  public async writeToHistory(text: string, companionKey: CompanionKey) {
+    if (!companionKey || typeof companionKey.userId == "undefined") {
+      console.log("Companion key set incorrectly");
+      return "";
+    }
+
+    const key = this.generateRedisCompanionKey(companionKey);
+    const result = await this.history.zadd(key, {
+      score: Date.now(),
+      member: text,
+    });
+
+    return result;
+  }
+}
